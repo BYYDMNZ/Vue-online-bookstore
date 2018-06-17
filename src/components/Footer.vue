b0e5a450ea2d58a6b8fbb80762d34477b1e5482b
BYYDMNZ <3164909398@qq.com>
2018-06-17 15:02:06 +0800

    接数据前

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer>
+        Copyright 2018 by YQ
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+footer{
+  width: 100%;
+  height: 50px;
+  bottom: 0;
+  text-align: center;
+  background: #eee;
+  color: #666;
+  font-size: 12px;
+  letter-spacing: 1px;
+  line-height: 50px;
+  position: relative;
+}
+</style>
